d9207d6baeb79a5739abee5da73d46e05e2d59db
kakashimaker <87550718+K4K4SH1M4K3R@users.noreply.github.com>
2021-09-17 12:23:44 -0300

    Create lh.py

---
+++ lh.py
@@ -0,0 +1,6 @@
+import os
+print ("BEM VINDO AO PEGA IP DA LOS HERMANOS")
+print (" https://check-host.net copie e cole no Chrome  ")
+print("e dps de abrir coloque na barra de pegar ip o site que vc quiser e dps clique em info e pronto obg :) ")
+os.system(x)
+print(" Obrigado por usar o ping ꧁☯️𝕷Ø𝕾.𝐇𝐄𝐑𝐌𝐀𝐍𝐎𝐒☯️꧂! ") 
